5c55f94a2417a53fdc6d87f07482cfb07ad88f59
Igor Korshunov <igor.9206@gmail.com>
2023-06-05 15:59:25 +0600

    add hw7 tas1 and task2

--- src/test/kotlin/WallServiceTest.kt
+++ src/test/kotlin/WallServiceTest.kt
@@ -62,4 +62,47 @@ class WallServiceTest {
         val result = WallService.update(newPost3)
         assertFalse(result)
     }
+
+    @Test
+    fun createCommentCorrect() {
+        val post1 = Post(ownerId = 1, fromId = 1, date = 22052023, text = "first", likes = Likes(), comments = Comments(), geo = Geo(place = Place()),
+            copyright = Copyright(), donut = Donut(placeholder = Placeholder()), postSource = PostSource(), repost = Repost(),
+            views = Views())
+        val post2 = Post(ownerId = 2, fromId = 2, date = 22052023, text = "second", likes = Likes(), comments = Comments(), geo = Geo(place = Place()),
+            copyright = Copyright(), donut = Donut(placeholder = Placeholder()), postSource = PostSource(), repost = Repost(),
+            views = Views())
+        val post3 = Post(ownerId = 3, fromId = 3, date = 22052023, text = "thrid", likes = Likes(), comments = Comments(), geo = Geo(place = Place()),
+            copyright = Copyright(), donut = Donut(placeholder = Placeholder()), postSource = PostSource(), repost = Repost(),
+            views = Views())
+
+        WallService.add(post1)
+        WallService.add(post2)
+        WallService.add(post3)
+
+        val comment = Comment(1, 3,5062023,"my comment1", 1, 3)
+        val result = WallService.createComment(1, comment)
+
+        assertEquals(comment,result)
+    }
+
+    @Test(expected = PostNotFoundException::class)
+    fun createCommentUnCorrect() {
+        val post1 = Post(ownerId = 1, fromId = 1, date = 22052023, text = "first", likes = Likes(), comments = Comments(), geo = Geo(place = Place()),
+            copyright = Copyright(), donut = Donut(placeholder = Placeholder()), postSource = PostSource(), repost = Repost(),
+            views = Views())
+        val post2 = Post(ownerId = 2, fromId = 2, date = 22052023, text = "second", likes = Likes(), comments = Comments(), geo = Geo(place = Place()),
+            copyright = Copyright(), donut = Donut(placeholder = Placeholder()), postSource = PostSource(), repost = Repost(),
+            views = Views())
+        val post3 = Post(ownerId = 3, fromId = 3, date = 22052023, text = "thrid", likes = Likes(), comments = Comments(), geo = Geo(place = Place()),
+            copyright = Copyright(), donut = Donut(placeholder = Placeholder()), postSource = PostSource(), repost = Repost(),
+            views = Views())
+
+        WallService.add(post1)
+        WallService.add(post2)
+        WallService.add(post3)
+
+        val comment = Comment(1, 3,5062023,"my comment1", 1, 3)
+        val result = WallService.createComment(5, comment)
+
+    }
 }
